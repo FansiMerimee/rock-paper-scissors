53ad447efe9eb7322602ebf98f11126dc5895edc
FansiMerimee <32071847+FansiMerimee@users.noreply.github.com>
2023-01-19 19:50:19 +0100

    Add comments to guide future developers who uses my code

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+
+// Create function to make computer play
 function getComputerChoice() {
     let arrChoices = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random()*arrChoices.length);
     return arrChoices[index];
 }
 
+// Create aj function to get the player's choice
 function getPlayerChoice(){
     let playerChoice = prompt("Enter your choice: rock, paper or scissors?");
     playerChoice = playerChoice.toLowerCase();
     return playerChoice;
 }
 
-
+// Store the player selection and computer selection into a variable
 const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
 
+// Create a function for one round game 
+function playRound(playerSelection, computerSelection) {
+    // your code here!
 
+}
 
