71113b60781ca0d1f0cc6f3b7354125c96615c73
FansiMerimee <32071847+FansiMerimee@users.noreply.github.com>
2023-01-19 20:19:17 +0100

    Create a new function 'playRound' that plays a single round of rock-paper-scissors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function getComputerChoice() {
     return arrChoices[index];
 }
 
-// Create aj function to get the player's choice
+// Create a function to get the player's choice
 function getPlayerChoice(){
     let playerChoice = prompt("Enter your choice: rock, paper or scissors?");
     playerChoice = playerChoice.toLowerCase();
@@ -19,7 +19,34 @@ const playerSelection = getPlayerChoice();
 
 // Create a function for one round game 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return "It's a draw! you both played Rock ";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerSelection == "paper" && computerSelection == "paper") {
+        return "It's a draw! you both played Paper ";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats paper";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beat Paper";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return "It's a draw! you both played Scissors";
+    }
 }
+
+
 
